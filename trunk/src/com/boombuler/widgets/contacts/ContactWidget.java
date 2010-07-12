71443145b98dfd68b8af7e35c9dced75692a3aa1
boombuler <boombuler@5c8a5c8a-79af-f76c-4cca-5d2e4fc705d8>
2010-07-12 18:26:06 +0000

    - added some other widget sizes (fixed issue 3)

--- trunk/src/com/boombuler/widgets/contacts/ContactWidget.java
+++ trunk/src/com/boombuler/widgets/contacts/ContactWidget.java
@@ -44,7 +44,7 @@ public class ContactWidget extends AppWidgetProvider {
 		Log.d(TAG, "recieved onUpdate");
 
         // Construct views
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main);
+        RemoteViews views = new RemoteViews(context.getPackageName(), getMainLayoutId());
         
         // Tell the widget manager
         final int N = appWidgetIds.length;
@@ -52,6 +52,11 @@ public class ContactWidget extends AppWidgetProvider {
             int appWidgetId = appWidgetIds[i];
             appWidgetManager.updateAppWidget(appWidgetId, views);            
         }	
+	}
+	
+	public int getMainLayoutId()
+	{
+		return R.layout.main;
 	}
 	
 	@Override
@@ -67,7 +72,7 @@ public class ContactWidget extends AppWidgetProvider {
 		} else if (TextUtils.equals(action, LauncherIntent.Action.ACTION_READY)) {
 			// Receive ready signal
 			Log.d(TAG, "widget ready");
-			ListViewManager.onAppWidgetReady(context, intent);
+			onAppWidgetReady(context, intent);
 		} else if (TextUtils.equals(action, LauncherIntent.Action.ACTION_FINISH)) {
 
 		} else if (TextUtils.equals(action, LauncherIntent.Action.ACTION_ITEM_CLICK)) {
@@ -118,5 +123,123 @@ public class ContactWidget extends AppWidgetProvider {
 		}
 	}
 	
+	
+	/**
+	 * Receive ready intent from Launcher, prepare scroll view resources
+	 */
+	public void onAppWidgetReady(Context context, Intent intent) {
+		if (intent == null)
+			return;
+
+		// try new method
+		int appWidgetId = intent.getExtras().getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
+				AppWidgetManager.INVALID_APPWIDGET_ID);
+
+		if (appWidgetId < 0) {
+			return;
+		}
+		Intent replaceDummy = CreateMakeScrollableIntent(appWidgetId);
+
+		// Send it out
+		context.sendBroadcast(replaceDummy);
+	}
+	
+	public Intent CreateMakeScrollableIntent(int appWidgetId) {
+		Log.d(TAG, "creating ACTION_SCROLL_WIDGET_START intent");
+		Intent result = new Intent(LauncherIntent.Action.ACTION_SCROLL_WIDGET_START);
+
+		// Put widget info
+		result.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+		result.putExtra(LauncherIntent.Extra.EXTRA_VIEW_ID, R.id.widget_content);
+
+		result.putExtra(LauncherIntent.Extra.Scroll.EXTRA_DATA_PROVIDER_ALLOW_REQUERY, true);
+
+		// Give a layout resource to be inflated. If this is not given, Home++
+		// will create one
+
+		// Put adapter info
+		result.putExtra(LauncherIntent.Extra.Scroll.EXTRA_LISTVIEW_LAYOUT_ID,
+				R.layout.listview);
+		result.putExtra(LauncherIntent.Extra.Scroll.EXTRA_ITEM_LAYOUT_ID, getListEntryLayoutId());
+		putProvider(result, DataProvider.CONTENT_URI_MESSAGES.buildUpon().appendEncodedPath(
+				Integer.toString(appWidgetId)).toString());
+		putMapping(result);
+
+		// Launcher can set onClickListener for each children of an item. Without
+		// explicitly put this
+		// extra, it will just set onItemClickListener by default
+		result.putExtra(LauncherIntent.Extra.Scroll.EXTRA_ITEM_CHILDREN_CLICKABLE, true);
+		return result;
+	}
+	
+	public int getListEntryLayoutId() {
+		return R.layout.contactlistentry;
+	}
+	
+	/**
+	 * Put provider info as extras in the specified intent
+	 * 
+	 * @param intent
+	 */
+	public void putProvider(Intent intent, String widgetUri) {
+		if (intent == null)
+			return;
+
+		String whereClause = null;
+		String orderBy = null;
+		String[] selectionArgs = null;
+
+		// Put the data uri in as a string. Do not use setData, Home++ does not
+		// have a filter for that
+		intent.putExtra(LauncherIntent.Extra.Scroll.EXTRA_DATA_URI, widgetUri);
+
+		// Other arguments for managed query
+		intent.putExtra(LauncherIntent.Extra.Scroll.EXTRA_PROJECTION, DataProvider.PROJECTION_APPWIDGETS);
+		intent.putExtra(LauncherIntent.Extra.Scroll.EXTRA_SELECTION, whereClause);
+		intent.putExtra(LauncherIntent.Extra.Scroll.EXTRA_SELECTION_ARGUMENTS, selectionArgs);
+		intent.putExtra(LauncherIntent.Extra.Scroll.EXTRA_SORT_ORDER, orderBy);
+
+	}
+
+	/**
+	 * Put mapping info as extras in intent
+	 */
+	public void putMapping(Intent intent) {
+		if (intent == null)
+			return;
+
+		final int NB_ITEMS_TO_FILL = 2;
+
+		int[] cursorIndices = new int[NB_ITEMS_TO_FILL];
+		int[] viewTypes = new int[NB_ITEMS_TO_FILL];
+		int[] layoutIds = new int[NB_ITEMS_TO_FILL];
+		boolean[] clickable = new boolean[NB_ITEMS_TO_FILL];
+		int[] defResources = new int[NB_ITEMS_TO_FILL];
+
+		int iItem = 0;
+		
+		intent.putExtra(LauncherIntent.Extra.Scroll.EXTRA_ITEM_ACTION_VIEW_URI_INDEX, 
+				DataProvider.DataProviderColumns.lookupkey.ordinal());
+		
+		cursorIndices[iItem] = DataProvider.DataProviderColumns.photo.ordinal();
+		viewTypes[iItem] = LauncherIntent.Extra.Scroll.Types.IMAGEBLOB;
+		layoutIds[iItem] = R.id.photo;
+		clickable[iItem] = true;
+		defResources[iItem] = R.drawable.identity;
+
+		iItem++;
+		
+		cursorIndices[iItem] = DataProvider.DataProviderColumns.name.ordinal();
+		viewTypes[iItem] = LauncherIntent.Extra.Scroll.Types.TEXTVIEW;
+		layoutIds[iItem] = R.id.displayname;
+		clickable[iItem] = false;
+		defResources[iItem] = 0;
+
+		intent.putExtra(LauncherIntent.Extra.Scroll.Mapping.EXTRA_VIEW_IDS, layoutIds);
+		intent.putExtra(LauncherIntent.Extra.Scroll.Mapping.EXTRA_VIEW_TYPES, viewTypes);
+		intent.putExtra(LauncherIntent.Extra.Scroll.Mapping.EXTRA_VIEW_CLICKABLE, clickable);
+		intent.putExtra(LauncherIntent.Extra.Scroll.Mapping.EXTRA_CURSOR_INDICES, cursorIndices);
+		intent.putExtra(LauncherIntent.Extra.Scroll.Mapping.EXTRA_DEFAULT_RESOURCES, defResources);
+	}
     
 }
