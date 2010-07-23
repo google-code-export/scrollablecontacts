5ff26ccf0f8c82f79d803ecfe28d21dfe9a1d51d
boombuler <boombuler@5c8a5c8a-79af-f76c-4cca-5d2e4fc705d8>
2010-07-23 15:43:09 +0000

    - moved the widget sizes to own package

---
+++ trunk/src/com/boombuler/widgets/contacts/sizes/ContactWidget_1_2.java
@@ -0,0 +1,5 @@
+package com.boombuler.widgets.contacts.sizes;
+
+public class ContactWidget_1_2 extends ContactWidget_1_X{
+
+}
---
+++ trunk/src/com/boombuler/widgets/contacts/sizes/ContactWidget_1_4.java
@@ -0,0 +1,5 @@
+package com.boombuler.widgets.contacts.sizes;
+
+public class ContactWidget_1_4 extends ContactWidget_1_X {
+
+}
